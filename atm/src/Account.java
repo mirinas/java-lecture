8a8d52d95cdad106b9e0ece47bfde335797f217f
mirinas <augustas.mirinas@gmail.com>
2025-01-14 15:45:34 +0200

    created Account class

---
+++ atm/src/Account.java
@@ -0,0 +1,44 @@
+
+public class Account {
+
+    private String accNumber;
+    private static int nextAccNumber = 101;
+
+    private String fullName;
+    private String pin;
+    private double balance = 0;
+
+    public Account(String fullName, String pin) {
+        this.fullName = fullName;
+        this.pin = pin;
+
+        accNumber = "LT" + nextAccNumber;
+        nextAccNumber++;
+    }
+
+
+    public boolean withdraw(double amount) {
+        if(amount > balance || amount < 0) {
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+
+
+    public double deposit(double amount) {
+        if(amount < 0) return balance;
+
+//        balance = balance + amount;
+        balance += amount;
+        return balance;
+    }
+
+
+    public void print() {
+        System.out.println("------------------");
+        System.out.printf("%s - %s\n", fullName, accNumber);
+        System.out.printf("Balance: %seur\n", balance);
+    }
+
+}
---
+++ atm/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Account acc1 = new Account("Augustas", "abcd");
+        Account acc2 = new Account("Petras", "xyz");
+
+
+    }
+}
