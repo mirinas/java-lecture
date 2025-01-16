e0f0ee266a2238380522e6f0c671a2c447e83f2c
mirinas <augustas.mirinas@gmail.com>
2025-01-16 11:54:41 +0200

    added ArrayList

--- atm/src/Main.java
+++ atm/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -6,28 +7,39 @@ public class Main {
         Account acc1 = new Account("Augustas", "abcd");
         Account acc2 = new Account("Petras", "xyz");
 
+        ArrayList<Account> accounts = new ArrayList<>();
+        accounts.add(acc1);
+        accounts.add(acc2);
+
 
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a command: deposit, withdraw, print, quit");
+
+        Account selectedAcc = null;
         while(true) {
 
             String cmd = s.next();
             switch (cmd) {
+                case "login" -> {
+                    int index = s.nextInt();
+                    selectedAcc = accounts.get(index);
+                    selectedAcc.print();
+                }
                 case "deposit" -> {
                     double amount = s.nextDouble();
-                    double newBalance = acc1.deposit(amount);
+                    double newBalance = selectedAcc.deposit(amount);
                     System.out.println("Your new balance: " + newBalance + "eur");
                 }
                 case "withdraw" -> {
                     double amount = s.nextDouble();
 
-                    if(acc1.withdraw(amount)) {
-                        System.out.println("Your new balance: " + acc1.getBalance() + "eur");
+                    if(selectedAcc.withdraw(amount)) {
+                        System.out.println("Your new balance: " + selectedAcc.getBalance() + "eur");
                     } else {
                         System.err.println("Insufficient funds!");
                     }
                 }
-                case "print" -> acc1.print();
+                case "print" -> selectedAcc.print();
                 case "quit" -> {
                     System.out.println("Bye bye!");
                     return;
