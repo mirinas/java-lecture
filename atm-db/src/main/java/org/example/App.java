57ed75c9b45e26207556f53a05f55975dcc86840
mirinas <augustas.mirinas@gmail.com>
2025-01-17 14:32:12 +0200

    try catch added

---
+++ atm-db/src/main/java/org/example/App.java
@@ -0,0 +1,103 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        Scanner s = new Scanner(System.in);
+
+        Account acc1 = new Account("Augustas", "abcd");
+        Account acc2 = new Account("Petras", "xyz");
+
+        HashMap<String, Account> accounts = new HashMap<>();
+        accounts.put(acc1.getAccNumber(), acc1);
+        accounts.put(acc2.getAccNumber(), acc2);
+
+
+        Account selectedAcc = null;
+        System.out.println("Login to an account: " + accounts.keySet());
+
+        String accNumber = s.next();
+        System.out.print("Enter PIN: ");
+        String pin1 = s.next();
+        Account acc0 = accounts.get(accNumber);
+        if(acc0.login(pin1)) {
+            selectedAcc = accounts.get(accNumber);
+            selectedAcc.print();
+        } else {
+            System.err.println("Incorrect PIN!");
+            return;
+        }
+
+
+        System.out.println("Enter a command: deposit, withdraw, print, quit");
+        while(true) {
+
+            String cmd = s.next();
+            switch (cmd) {
+                case "login" -> {
+                    System.out.println("Login to an account: " + accounts.keySet());
+
+                    String accNo = s.next();
+                    System.out.print("Enter PIN: ");
+                    String pin = s.next();
+
+                    Account loggedIn = accounts.get(accNo);
+                    // jeigu prie sito acc galima prisijungti suvestu slaptazodziu
+                    if(loggedIn.login(pin)) {
+                        selectedAcc = loggedIn;
+                        selectedAcc.print();
+                    } else {
+                        System.err.println("Incorrect PIN");
+                    }
+
+                }
+                case "register" -> {
+                    String name = s.next();
+                    String pin = s.next();
+
+                    Account newAcc = new Account(name, pin);
+
+                    accounts.put(newAcc.getAccNumber(), newAcc);
+                }
+                case "deposit" -> {
+
+                    try {
+                        double amount = s.nextDouble();
+                        double newBalance = selectedAcc.deposit(amount);
+                        System.out.println("Your new balance: " + newBalance + "eur");
+
+                    } catch (InputMismatchException e) {
+                        System.err.println("Deposit amount must be a number");
+                    }
+
+                }
+                case "withdraw" -> {
+                    try {
+                        double amount = s.nextDouble();
+
+                        if (selectedAcc.withdraw(amount)) {
+                            System.out.println("Your new balance: " + selectedAcc.getBalance() + "eur");
+                        } else {
+                            System.err.println("Insufficient funds!");
+                        }
+                    } catch (InputMismatchException e) {
+                        System.err.println("Deposit amount must be a number");
+                    }
+                }
+                case "print" -> selectedAcc.print();
+                case "quit" -> {
+                    System.out.println("Bye bye!");
+                    return;
+                }
+//                default -> System.err.println("Invalid command. Available: quit");
+            }
+        }
+    }
+}
